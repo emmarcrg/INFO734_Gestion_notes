a4f097e60786b4b5cdb96e67f7a8beb75066efb6
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-11-14 15:03:59 +0100

    Ajout classe + correction

---
+++ gestion_note/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+// src/app/login/login.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  handleSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
