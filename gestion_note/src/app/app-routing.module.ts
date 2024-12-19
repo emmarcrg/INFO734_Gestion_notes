40015dfad560ef7a2618eb40f97a581ba9ca076a
Axelle <160240449+aquellery@users.noreply.github.com>
2024-12-19 12:53:31 +0100

    affichage des notes sous forme de tableau
    reste à faire : lien mongo

--- gestion_note/src/app/app-routing.module.ts
+++ gestion_note/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SaisieComponent } from './page_saisie_note/saisie_note.component';
+import { AffichageComponent} from './page_affichage_notes/affichage_note.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/page_saisie_note', pathMatch: 'full' },  // Route par défaut qui redirige vers 'login'
+  { path: '', redirectTo: '/page_affichage_notes', pathMatch: 'full' },  // Route par défaut qui redirige vers 'login'
   { path: 'login', component: LoginComponent },  // Route pour la page de connexion
   { path: 'page_saisie_note', component: SaisieComponent },  // Route pour la page de saisie des notes
+  { path: 'page_affichage_notes', component: AffichageComponent },  // Route
 ];
 
 @NgModule({
