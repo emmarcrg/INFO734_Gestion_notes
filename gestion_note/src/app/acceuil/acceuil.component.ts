626396b8022135a82fa8223af3ee44983f535f7e
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-12-29 16:43:48 +0100

    Commit pour update angular

--- gestion_note/src/app/acceuil/acceuil.component.ts
+++ gestion_note/src/app/acceuil/acceuil.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-acceuil',
-  templateUrl: './acceuil.component.html',
-  styleUrls: ['./acceuil.component.css'],
-  standalone: true,
-  imports: [CommonModule]
+    selector: 'app-acceuil',
+    templateUrl: './acceuil.component.html',
+    styleUrls: ['./acceuil.component.css'],
+    imports: [CommonModule]
 })
 export class AcceuilComponent {}
--- gestion_note/src/app/app.component.ts
+++ gestion_note/src/app/app.component.ts
@@ -6,10 +6,9 @@ import { LoginComponent } from './login/login.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, LoginComponent, AcceuilComponent]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, FormsModule, RouterModule, LoginComponent, AcceuilComponent]
 })
 export class AppComponent {}
