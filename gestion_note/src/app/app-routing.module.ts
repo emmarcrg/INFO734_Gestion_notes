fa24f94d204aed38977523c375b88ddebc5cc4b9
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2025-01-07 17:45:15 +0100

    Commit pour merge

--- gestion_note/src/app/app-routing.module.ts
+++ gestion_note/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { AccueilComponent } from './page_accueil/accueil/accueil.component';
 import { SignupComponent } from './signup/signup.component';
 
+//NE SERT A RIENNNNNNNNNNNNNNNNNNNNNN
+
 const routes: Routes = [
   // { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
