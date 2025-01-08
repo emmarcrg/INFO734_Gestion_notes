9aec41e3fb8cdc7f1ac241e86b9d1e0b80e374e3
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2025-01-08 16:50:06 +0100

    Accès à l'utilisateur depuis l'acceuil

--- gestion_note/src/app/login/login.component.ts
+++ gestion_note/src/app/login/login.component.ts
@@ -2,39 +2,47 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ConnexionService } from '../connexion.service';  
+import { ConnexionService } from '../connexion.service';
 import { User } from './user';
+import { UserService } from '../user.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
-    standalone : true,
-    imports: [CommonModule, FormsModule]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private connexionService: ConnexionService, private router: Router) {}
+  constructor(
+    private connexionService: ConnexionService,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   onLogin() {
     this.connexionService.getPersonneByLogin(this.email).subscribe({
       next: (response) => {
         console.log('Logged in successfully:', response);
-        //On enregistre un objet de type user lors de la connexion => garder en mémoire la personne connectée
-        const user: User = { 
-          identifiant: response.user._id, 
+
+        const user: User = {
+          identifiant: response.user._id,
           nom: response.user.nom,
           prenom: response.user.prenom,
-          semestre: '0' };
-        console.log('La personne ' + user.nom + ' ' +user.prenom + ' est connectée');
+          semestre: '0',
+        };
+        console.log('La personne ' + user.nom + ' ' + user.prenom + ' est connectée');
+        
+        this.userService.setUser(user);
         this.navigateToAcceuil();
       },
       error: (error) => {
         console.error('Error logging in:', error);
         alert('Invalid username or password');
-      }
+      },
     });
   }
 
@@ -42,8 +50,7 @@ export class LoginComponent {
     this.router.navigate(['/signup']);
   }
 
-  navigateToAcceuil(){
-    
+  navigateToAcceuil() {
     this.router.navigate(['/accueil']);
   }
 }
--- gestion_note/src/app/page_accueil/accueil/accueil.component.ts
+++ gestion_note/src/app/page_accueil/accueil/accueil.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../user.service'; // Chemin relatif vers le service UserService
+import { User } from '../../login/user'; // Chemin relatif vers l'interface User
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.css'
+  styleUrls: ['./accueil.component.css'],
 })
 export class AccueilComponent {
+  user: User | null = null;
 
+  constructor(private userService: UserService) {
+    this.user = this.userService.getUser();
+  }
 }
